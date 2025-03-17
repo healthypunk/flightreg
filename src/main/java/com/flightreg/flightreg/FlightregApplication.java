439c98b9af15b8b537e67fb04429ce984c0602f7
healthypunk <73522653+evaldaspetnjunas@users.noreply.github.com>
2025-03-17 18:38:27 +0200

    Add initialized repository

---
+++ src/main/java/com/flightreg/flightreg/FlightregApplication.java
@@ -0,0 +1,13 @@
+package com.flightreg.flightreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightregApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flightreg/flightreg/FlightregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightreg.flightreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightregApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
