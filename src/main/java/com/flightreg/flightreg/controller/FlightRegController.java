f368f3b39ee9a4e728905d73ede220910bf6d9a1
petnj <evaldas.petnjunas@ut.ee>
2025-03-30 10:56:21 +0300

    Added seat initialization on flight creation and added mappings to controller

--- src/main/java/com/flightreg/flightreg/controller/FlightRegController.java
+++ src/main/java/com/flightreg/flightreg/controller/FlightRegController.java
@@ -1,27 +1,96 @@
 package com.flightreg.flightreg.controller;
 
-import com.flightreg.flightreg.model.Flight;
+import com.flightreg.flightreg.model.*;
 import com.flightreg.flightreg.service.FlightRegService;
+import com.flightreg.flightreg.service.ReservationService;
+import com.flightreg.flightreg.service.SeatSuggestionService;
+import com.flightreg.flightreg.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/flights")
+@RequestMapping("/api")
 public class FlightRegController {
     @Autowired
     private FlightRegService flightRegService;
 
-    @GetMapping
+    @Autowired
+    private SeatSuggestionService seatSuggestionService;
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/flights")
     public List<Flight> getFlights(){
         return flightRegService.getAllFlights();
     }
 
-    @PostMapping
+    @PostMapping("/flights")
     public void saveFlight(@RequestBody Flight flight){
-        flightRegService.saveFlight(flight);
+        Flight savedFlight = flightRegService.saveFlight(flight);
+
+        flightRegService.initializeSeats(savedFlight.getId(),90);
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(
+            @RequestParam String username,
+            @RequestParam String email,
+            @RequestParam String password) {
+        User newUser = userService.createUser(username, email, password);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping("/seats/suggestions")
+    public ResponseEntity<List<Seat>> recommendSeats(
+            @RequestParam int flightId,
+            @RequestParam int numOfSeats,
+            @RequestParam(required = false) List<SeatType> seatTypes) {
+
+        List<Seat> suggestedSeats = seatSuggestionService.recommendSeats(flightId, numOfSeats, seatTypes);
+
+        return ResponseEntity.ok(suggestedSeats);
+    }
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getUserReservations(int userId){
+        return ResponseEntity.ok(reservationService.getUserReservations(userId));
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> reserveSeats(
+            @RequestParam int userId,
+            @RequestParam int flightId,
+            @RequestParam List<Integer> seatIds) {
+
+        User user = new User();  // TODO: Fetch from DB
+        Flight flight = new Flight();  // TODO: Fetch from DB
+
+        List<Seat> seats = seatSuggestionService.getAllSeats()
+                .stream()
+                .filter(seat -> seatIds.contains(seat.getId()))
+                .toList();
+
+        // Reserve seats
+        Reservation reservation = reservationService.reserveSeat(user, flight, seats);
+        return ResponseEntity.ok(reservation);
+    }
+
+    // Cancel a reservation
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable int id) {
+        reservationService.deleteReservation(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 
--- src/main/java/com/flightreg/flightreg/service/FlightRegService.java
+++ src/main/java/com/flightreg/flightreg/service/FlightRegService.java
@@ -1,7 +1,12 @@
 package com.flightreg.flightreg.service;
 
 import com.flightreg.flightreg.model.Flight;
+import com.flightreg.flightreg.model.Seat;
+import com.flightreg.flightreg.model.SeatType;
 import com.flightreg.flightreg.repository.FlightRepository;
+import com.flightreg.flightreg.repository.SeatRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +18,16 @@ public class FlightRegService {
     @Autowired
     private FlightRepository flightRepository;
 
+    @Autowired
+    private SeatRepository seatRepository;
+
     public List<Flight> getAllFlights(){
         List<Flight> flightList = new ArrayList<>();
         flightRepository.findAll().forEach(flightList::add);
         return flightList;
     }
 
-    public void saveFlight(Flight flight){
+    public Flight saveFlight(Flight flight){
         var builder = Flight.builder()
                 .destination(flight.getDestination())
                         .departurePoint(flight.getDeparturePoint())
@@ -30,7 +38,43 @@ public class FlightRegService {
                                                                 .arrivalTime(flight.getArrivalTime())
                                                                         .price(flight.getPrice())
                                                                                 .availableSeats(flight.getAvailableSeats());
-        flightRepository.save(builder.build());
+        return flightRepository.save(builder.build());
+    }
+
+    public void initializeSeats(int flightId, int numberOfSeats) {
+        List<Seat> seats = new ArrayList<Seat>();
+
+        Flight flight = flightRepository.findById(flightId)
+                .orElseThrow(() -> new EntityNotFoundException("Flight not found with id: " + flightId));
+
+        int seatsPerRow = 6;
+        int totalRows = numberOfSeats / seatsPerRow;
+
+        for (int i = 0; i < numberOfSeats; i++) {
+            int rowNumber = (i / seatsPerRow) + 1;
+            char seatLetter = (char) ('A' + (i % seatsPerRow));
+
+            SeatType seatType;
+            if (rowNumber == 1 || rowNumber == totalRows) {
+                seatType = SeatType.NEAR_EXIT;
+            } else if (seatLetter == 'A' || seatLetter == 'F') {
+                seatType = SeatType.WINDOW;
+            } else if (seatLetter == 'C' || seatLetter == 'D') {
+                seatType = SeatType.AISLE;
+            } else {
+                seatType = SeatType.STANDARD;
+            }
+
+            Seat seat = Seat.builder()
+                    .seatNumber(rowNumber)
+                    .seatLetter(seatLetter)
+                    .seatType(seatType)
+                    .isOccupied(false)
+                    .flight(flight).build();
+            seats.add(seat);
+        }
+
+        seatRepository.saveAll(seats);
     }
 
 }
